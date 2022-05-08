e55af6ff726419db38d57eaf2e1b00db86281bef
lua <84481492+lua-cmd@users.noreply.github.com>
2022-05-08 18:55:40 -0300

    My finest creation yet.
    works like a fucking CHARM m8s

--- EpicFound.lua
+++ EpicFound.lua
@@ -3,50 +3,62 @@
 
 -- Instances:
 
-local Loader = Instance.new("ScreenGui")
-local EpicFound = Instance.new("Frame")
+local projectepicanimation = Instance.new("ScreenGui")
+local status = Instance.new("TextLabel")
 local epicface = Instance.new("ImageLabel")
-local statuslabel = Instance.new("TextLabel")
 
 --Properties:
 
-Loader.Name = "Loader"
-Loader.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-Loader.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+projectepicanimation.Name = "projectepicanimation"
+projectepicanimation.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+projectepicanimation.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-EpicFound.Name = "EpicFound"
-EpicFound.Parent = Loader
-EpicFound.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-EpicFound.BackgroundTransparency = 1.000
-EpicFound.Position = UDim2.new(0, 0, 0.80119282, 0)
-EpicFound.Size = UDim2.new(0, 100, 0, 100)
+status.Name = "status"
+status.Parent = projectepicanimation
+status.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+status.BackgroundTransparency = 1.000
+status.BorderSizePixel = 2
+status.Position = UDim2.new(0, 130, 0, 457)
+status.Size = UDim2.new(0, 216, 0, 100)
+status.Font = Enum.Font.SourceSansBold
+status.Text = "Project Epic made by papalua and ananymoos, use the external program to execute scripts                       Status: Loading..."
+status.TextColor3 = Color3.fromRGB(0, 0, 0)
+status.TextScaled = true
+status.TextSize = 18.000
+status.TextTransparency = 1.000
+status.TextWrapped = true
 
 epicface.Name = "epicface"
-epicface.Parent = EpicFound
+epicface.Parent = projectepicanimation
+epicface.AnchorPoint = Vector2.new(0.5, 0.5)
 epicface.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 epicface.BackgroundTransparency = 1.000
-epicface.Position = UDim2.new(0.129999995, 0, -0.550000012, 0)
+epicface.Position = UDim2.new(0, -50, 0, 251)
 epicface.Size = UDim2.new(0, 100, 0, 100)
 epicface.Image = "rbxassetid://518532451"
 
-statuslabel.Name = "statuslabel"
-statuslabel.Parent = EpicFound
-statuslabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-statuslabel.BorderSizePixel = 2
-statuslabel.Position = UDim2.new(1.13, 0, -0.550000012, 0)
-statuslabel.Size = UDim2.new(0, 215, 0, 100)
-statuslabel.Font = Enum.Font.GothamBold
-statuslabel.Text = "project epic trolled this game   status: injected"
-statuslabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-statuslabel.TextSize = 14.000
-statuslabel.TextWrapped = true
-
 -- Scripts:
 
-local function WFRBY_fake_script() -- EpicFound.LocalScript 
-	local script = Instance.new('LocalScript', EpicFound)
+local function ZLFDNW_fake_script() -- projectepicanimation.animator 
+	local script = Instance.new('LocalScript', projectepicanimation)
 
-	wait(45)
-	script.Parent.Parent:Destroy()
+	local epicface = script.Parent.epicface
+	epicface.AnchorPoint = Vector2.new(0.5, 0.5)
+	local status = script.Parent.status
+	local TweenService = game:GetService("TweenService")
+	local statusanim = TweenService:Create(status, TweenInfo.new(1), {BackgroundTransparency = 0})
+	local statustextanim = TweenService:Create(status, TweenInfo.new(1), {TextTransparency = 0})
+	
+	wait(2.5)
+	epicface:TweenPosition(UDim2.new(0.5,0,0.5,0))
+	wait(2.5)
+	epicface:TweenPosition(UDim2.new(0,62,0,507))
+	wait(1)
+	statusanim:Play()
+	statustextanim:Play()
+	
+	repeat wait(5) until game:IsLoaded()
+	wait(5)
+	status.Text = "Project Epic made by papalua and ananymoos, use the external program to execute scripts                          Status: Working"
 end
-coroutine.wrap(WFRBY_fake_script)()
+coroutine.wrap(ZLFDNW_fake_script)()
